c6004aa7beaeebf5f4c9c28b2df2942a521286ec
atang@upchain.com <atang@upchain.com>
2021-07-06 11:45:34 -0400

    able to toggle logging levels by using logback instead of slf4j

--- src/main/java/com/appsdeveloperblog/app/ws/testing/Controller.java
+++ src/main/java/com/appsdeveloperblog/app/ws/testing/Controller.java
@@ -1,17 +1,20 @@
 package com.appsdeveloperblog.app.ws.testing;
-import com.upchain.logging.LoggerFactory;
-import org.slf4j.Logger;
+//import com.upchain.logging.LoggerFactory;
+//import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.launchdarkly.sdk.*;
 import com.launchdarkly.sdk.server.*;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("test")
 public class Controller {
 
-    private static Logger logger = LoggerFactory.getLogger(Controller.class);
+    private static Logger logger = (Logger) LoggerFactory.getLogger(Controller.class);
     LDClient ldClient = new LDClient("sdk-9e47d71f-dc78-48ba-9e3c-4f6635bbd253");
 
     /*
@@ -31,6 +34,8 @@ public class Controller {
     @GetMapping
     public String getString(){
         LDUser user = new LDUser("user@test.com");
+        int toggleLevel = ldClient.intVariation("logger-level-toggle-test-andrew-qi-tang", user, Level.DEBUG.toInt());
+        logger.setLevel(Level.toLevel(toggleLevel));
         logger.debug("you should not see this");
         String s = ldClient.stringVariation("multivariate-string-flag-test-andrew-qi-tang", user, "");
         if(s.equals("")){
